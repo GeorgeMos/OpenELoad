c655c0d162c006196cfc55885be90edc0570aa47
GeorgeMos <georgemosxonas13@gmail.com>
2024-08-02 15:52:17 +0300

    Fixed GUI CMAKE

--- desktopApp/nanaGui/main/main.cpp
+++ desktopApp/nanaGui/main/main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
-#include "nana/gui.hpp"
-#include "usbCom.hpp"
-
-
+#include <nana/gui.hpp>
+#include <../../cpp_libs/inc/usbCom.hpp>
 int main()
 {
-    return 0;
+	//Brings all nana names into main scope.
+	//using namespace nana;
+
+	//Create a form.
+	nana::form fm;
+    
+	//Draws text "Hello World!" on the form.
+	nana::drawing{fm}.draw([](nana::paint::graphics& graph){
+		graph.string({ 10, 20 }, "Hello World!", nana::colors::black);
+	});
+
+	//Shows the form
+	fm.show();
+
+	//where main() passes the control to Nana, and exec() will return
+	//if the form is closed.
+	nana::exec();
+    
+   return 0;
 }
